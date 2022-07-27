07228089bad18bb623bfa778164bc9c2e7cb6fb9
Mederbekovaa <mederbekovaaiyma11@gmal.com>
2022-07-27 19:58:24 +0600

    fix

--- src/main/java/kg/itacademy/protection/configuration/SecurityConfig.java
+++ src/main/java/kg/itacademy/protection/configuration/SecurityConfig.java
@@ -1,11 +1,14 @@
 package kg.itacademy.protection.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -47,5 +50,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .httpBasic();
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
     }
 }
