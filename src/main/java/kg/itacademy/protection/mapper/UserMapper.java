f31dc37c6b7926dedec8f6b723dd28104261a3ba
Mederbekovaa <mederbekovaaiyma11@gmal.com>
2022-08-01 18:54:21 +0600

    fixe

--- src/main/java/kg/itacademy/protection/mapper/UserMapper.java
+++ src/main/java/kg/itacademy/protection/mapper/UserMapper.java
@@ -2,10 +2,12 @@ package kg.itacademy.protection.mapper;
 
 import kg.itacademy.protection.entity.UserEntity;
 import kg.itacademy.protection.model.UserModel;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
+@Mapper
 public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
